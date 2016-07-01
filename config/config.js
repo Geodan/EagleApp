7ee0bd1b0ab5306dcae9ca43206be818f921a6ab
Tom van Tilburg <tom.van.tilburg@gmail.com>
2016-07-01 14:17:06 +0200

    first commit with content

---
+++ config/config.js
@@ -0,0 +1,9 @@
+var config = {
+    cowconfig: {
+        url: 'sslgw.geodan.nl/eaglewater/geodan',
+        port: 443,
+        protocol: 'wss',
+        dir: 'geodan',
+        key: 'geodan'
+    }
+};
